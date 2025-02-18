e30925f1ae7e3646372c32cdf4a56780c6a00006
KevinterrYxX <kevinjhoan62@gmail.com>
2025-02-18 14:45:15 -0500

    Primeros ejercicios

---
+++ Ejercicios.kt
@@ -0,0 +1,22 @@
+fun main() {
+    println("I'm")
+    println("learning")
+    println("Kotlin!")
+}
+
+fun main() {
+    println("Tomorrow is rainy")
+}
+
+fun main() {
+    println("There is a chance of snow")
+}
+
+fun main() {
+    println("Cloudy") 
+    println("Partly Cloudy") 
+    println("Windy")
+}
+fun main() {
+    println("How's the weather today?")
+}
