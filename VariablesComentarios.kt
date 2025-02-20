5f08957aa532e6826505074b864ef0f70a3ee47d
KevinterrYxX <kevinjhoan62@gmail.com>
2025-02-20 13:07:01 -0500

    Se agrega al trabajo archivos variables

---
+++ VariablesComentarios.kt
@@ -0,0 +1,37 @@
+//Definir y actualizar variables
+fun main() {
+    val mensajesLeido = 100
+    val mensajesNoleidos = 5
+    println("Tu tienes ${mensajesLeido + mensajesNoleidos} mensajes en tu correo")
+}
+
+fun main() {
+    var carTotal: Int = 0
+    println("Tienes un total de: $carTotal en el carrito")
+
+    carTotal = 20
+    println("Tienes un total de: $carTotal en el carrito")
+}
+
+fun main() {
+    var conteo = 10
+    println("Tienes $conteo mensajes")
+    conteo-=1
+    println("Tienes $conteo mensajes")
+}
+
+fun main() {
+    val encuentro = "Proximo encuentro es el: "
+    val cita = "Marzo 1"
+    val frase = encuentro + cita
+    println(frase)
+}
+
+fun main() {
+    println("Say \"hello\"")
+}
+
+fun main() {
+    val notificacionesActivas: Boolean = false
+    println("Tieens las notificaciones activas? " + notificacionesActivas)
+}
