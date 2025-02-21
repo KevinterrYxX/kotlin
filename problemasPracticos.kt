eb37562ea90e5c7752b2f045ef0f5d98120ae715
KevinterrYxX <kevinjhoan62@gmail.com>
2025-02-20 19:40:05 -0500

    Ejercicios practicos

---
+++ problemasPracticos.kt
@@ -0,0 +1,104 @@
+fun main() {
+    println("Use the val keyword when the value doesn't change.")
+    println("Use the var keyword when the value can change.")
+    println("When you define a function, you define the parameters that can be passed to it")
+    println("When you call a function, you pass arguments for the parameters.")
+}
+
+fun main() {
+    println("New chat message from a friend")
+}
+
+fun main() {
+    var discountPercentage: Int
+    var offer: String
+    val item = "Google Chromecast"
+    discountPercentage = 20
+    offer = "Sale - Up to $discountPercentage% discount on $item! Hurry up!"
+
+    println(offer)
+}
+
+fun main() {
+    val numberOfAdults = 20
+    val numberOfKids = 30
+    val total = numberOfAdults + numberOfKids
+    println("The total party size is: $total")
+}
+
+fun main() {
+    val baseSalary = 5000
+    val bonusAmount = 1000
+    val totalSalary = "$baseSalary + $bonusAmount"
+    println("Congratulations for your bonus! You will receive a total of $totalSalary (additional bonus).")
+}
+
+fun main() {
+    val firstNumber = 10
+    val secondNumber = 5
+    val result = firstNumber + secondNumber
+
+    println("$firstNumber + $secondNumber = $result")
+}
+
+fun main() {
+    val firstNumber = 10
+    val secondNumber = 5
+    val thirdNumber = 8
+
+    val result = add(firstNumber, secondNumber)
+    val anotherResult = subtract(firstNumber, thirdNumber)
+
+    println("$firstNumber + $secondNumber = $result")
+    println("$firstNumber + $thirdNumber = $anotherResult")
+}
+
+// Define add() function below this line
+fun add(numero1: Int, numero2: Int): Int {
+    val sumatoria = numero1 + numero2
+    return sumatoria
+}
+
+fun subtract(numero1: Int, numero2: Int): Int {
+    val resta = numero1 - numero2
+    return resta
+}
+
+fun main() {
+    val firstUserEmailId = "user_one@gmail.com"
+
+    // The following line of code assumes that you named your parameter as emailId.
+    // If you named it differently, feel free to update the name.
+    println(displayAlertMessage(email = firstUserEmailId))
+    println()
+
+    val secondUserOperatingSystem = "Windows"
+    val secondUserEmailId = "user_two@gmail.com"
+
+    println(displayAlertMessage(secondUserOperatingSystem, secondUserEmailId))
+    println()
+
+    val thirdUserOperatingSystem = "Mac OS"
+    val thirdUserEmailId = "user_three@gmail.com"
+
+    println(displayAlertMessage(thirdUserOperatingSystem, thirdUserEmailId))
+    println()
+}
+
+// Define your displayAlertMessage() below this line.
+fun displayAlertMessage(sistemaOperativo: String = "Unknown OS", email: String): String {
+    val alerta = "There's a new sign-in request on $sistemaOperativo for your Google Account $email"
+    return alerta
+}
+
+fun main() {
+    val pasos = 4000
+    val calorias = PEDOMETRO(pasos);
+    println("Walking $pasos steps burns $calorias calories")
+}
+
+fun PEDOMETRO(NumberOFStepS: Int): Double {
+    val CaloriesBURNEDforEachStep = 0.04
+    val TotalCALORIESburned = NumberOFStepS * CaloriesBURNEDforEachStep
+    return TotalCALORIESburned
+}
